fa20d67c170ca65bd698f28bec2229510edc425c
tushar singh <72065122+tusharsgh@users.noreply.github.com>
2023-10-15 04:19:07 +0530

    message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import TaxCal from './components/TaxCal/TaxCal';
 import {items} from "./text";
+
+
 function App() {
 
   console.log(items);
@@ -11,7 +13,9 @@ function App() {
      <div className="column">
      <TaxCal/>  
 <div className="FAQ">
-<div className="text"></div>
+<div className="text">
+  {items[0].answer}
+</div>
   </div>
 </div>
       </div>
--- src/components/TaxCal/TaxCal.jsx
+++ src/components/TaxCal/TaxCal.jsx
@@ -1,6 +1,8 @@
 import React,{useState} from 'react'
 import  './TaxCal.css';
-import Input from '../Input/Input'
+import Input from '../Input/Input';
+import { faSolid, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function TaxCal() {
   const [isshortTerm,setisshortTerm]=useState("false");
   const[islongterm,setislongterm]=useState("false");
@@ -9,31 +11,35 @@ function TaxCal() {
        <div className="heading">Free Crypto Tax Calculator</div>  
        <div className="line"></div>  
        <div className="subContainer">
-        <div className="subcontainer1">
-         <Input name="Enter purchase price of Crypto"/>
-         <Input name="Enter your Expenses"/>
-         <Input name="Select Your Annual Income"/>
-         <Input name="Capital gains amount"/>
-
-         </div>
-<div className="subcontainer2">
-<Input name="Enter sale price of Crypto"/>
-<div className="investheading"> Investment type</div>
-<div className="button">
-
-
-  <button>
-
-  </button>
-
-</div>
-<Input name="Discount for long term gains"/>
-</div>
-</div>
+        <div className="subContainer1">
+        <Input name="Enter the purchase price of crypto"/>
+        <Input name="Enter sale price of crypto"/>
         </div>
+        <div className="subContainer2">
+        <Input name="Enter your expense"/>
+        <div className="intype">
+          <div className="header">Investment Type</div>
+          <div className="alignbtn">
+          <button className="btn"> Short Term </button>
+          <button className="btn"> Long Term</button>
+          </div>
+        </div>
+        </div>
+        <div className="subContainer1">
+        <Input name="Select Your Annual Income"/>
+        
+        </div>
+
+       
+
+      
+
+
+</div>
+        
       
        
-        
+   </div>     
     
   )
 }
